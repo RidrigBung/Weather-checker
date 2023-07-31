58f610e564f7b4d3d90a20f2b5585259bf125e20
= <=>
2023-07-31 18:13:32 +0300

    Создан класс City

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from get_weather_variables import today, hours, week
 from get_weather_json import city
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        pass
+        # latitude = request.form.get("latitude")
+        # longitude = request.form.get("longitude")
     return render_template('index.html', weather_url=url_for("weather"))
 
 
 @app.route("/weather")
 def weather():
-    return render_template('weather.html', city=city["name"], today=today, hours=hours, week=week)
+    return render_template('weather.html', city=city.name, today=today, hours=hours, week=week)
 
 
 if __name__ == "__main__":
--- get_weather_json.py
+++ get_weather_json.py
@@ -2,13 +2,26 @@ import requests
 from datetime import date
 import json
 
+
+class City:
+    latitude: float
+    longitude: float
+    name: str
+
+    def __init__(self, latitude: float, longitude: float, name: str) -> None:
+        self.latitude = latitude
+        self.longitude = longitude
+        self.name = name
+
+
 base_url = "https://api.open-meteo.com/v1/forecast?"
 
 city = {"name": "Dmitrov", "latitude": "56.34",
         "longitude": "37.52", "timezone": "Europe%2FMoscow"}
+city = City(56.34, 37.52, "Dmitrov")
 
 response = requests.get(
-    base_url + f"latitude={city['latitude']}&longitude={city['longitude']}&hourly=temperature_2m,precipitation_probability,weathercode,surface_pressure,windspeed_10m&timezone={city['timezone']}")
+    base_url + f"latitude={city.latitude}&longitude={city.longitude}&hourly=temperature_2m,precipitation_probability,weathercode,surface_pressure,windspeed_10m&timezone={city['timezone']}")
 print(f"https get response: {response}")
 weather_dict = response.json()
 
