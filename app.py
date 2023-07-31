0258520655bb9b174cab730c045bbe95c6a9a04f
= <=>
2023-07-31 17:42:01 +0300

    Перенес данные index.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from get_weather_variables import today, hours, week
 from get_weather_json import city
 
@@ -6,9 +6,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/index/")
 def index():
-    return render_template('index.html', city=city["name"], today=today, hours=hours, week=week)
+    return render_template('index.html', weather_url=url_for("weather"))
+
+
+@app.route("/weather")
+def weather():
+    return render_template('weather.html', city=city["name"], today=today, hours=hours, week=week)
 
 
 if __name__ == "__main__":
