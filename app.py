3f5f7d7ee60cc68103d4a6f6ad31ec9785be2c82
Andrew Babanin <baban1n.and@yandex.ru>
2023-08-02 22:28:42 +0300

    Теперь можно выбрать город или задать координаты

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request
 from datetime import date
 from weather_data_processing import get_current_weather_data
+from cities_load import get_cities
 from classes import City
 
 app = Flask(__name__)
@@ -9,12 +10,25 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        city = City(request.form.get("latitude"), request.form.get(
-            "longitude"), request.form.get("name"))
+        city_name = request.form.get("city_name")
+        if city_name == "":
+            latitude = request.form.get("latitude")
+            longitude = request.form.get("longitude")
+            city_name = "Coordinates"
+            city = City(latitude, longitude, city_name)
+            cur_date = date.today()
+            data = get_current_weather_data(city, cur_date)
+            return render_template('weather.html', city=city.name, today=data["today"], hours=data["hours"], week=data["week"])
+        for static_city in get_cities():
+            if static_city["name"] == city_name:
+                latitude = static_city["latitude"]
+                longitude = static_city["longitude"]
+                break
+        city = City(latitude, longitude, city_name)
         cur_date = date.today()
         data = get_current_weather_data(city, cur_date)
         return render_template('weather.html', city=city.name, today=data["today"], hours=data["hours"], week=data["week"])
-    return render_template('index.html')
+    return render_template('index.html', cities=get_cities())
 
 
 if __name__ == "__main__":
--- cities_load.py
+++ cities_load.py
@@ -1,16 +1,16 @@
 import csv
+from typing import List
 
 
-def get_cities() -> list:
+def get_cities() -> List[dict]:
     cities = []
     with open("./static/cities_table.csv", "r") as table:
         reader = csv.reader(table, delimiter=",")
         for city in reader:
             cities.append(
-                {"name": city[0], "latitude": city[1], "longitude": city[2]})
-    print(cities)
+                {"name": city[0], "latitude": float(city[1]), "longitude": float(city[2])})
     return cities
 
 
 if __name__ == "__main__":
-    get_cities()
+    print(get_cities())
