3f3fb432ce8ec98ed80a21d29f70c939821be283
Andrew Babanin <baban1n.and@yandex.ru>
2023-08-02 21:29:40 +0300

    cities теперь представлен в виде словаря

--- cities_load.py
+++ cities_load.py
@@ -3,7 +3,14 @@ import csv
 
 def get_cities() -> list:
     cities = []
-    with open("cities_table.csv", "r") as table:
+    with open("./static/cities_table.csv", "r") as table:
         reader = csv.reader(table, delimiter=",")
-        cities = [city for city in reader]
+        for city in reader:
+            cities.append(
+                {"name": city[0], "latitude": city[1], "longitude": city[2]})
+    print(cities)
     return cities
+
+
+if __name__ == "__main__":
+    get_cities()
